09fe4984a0de8d26c43f8f37261f98286af4c94a
emmateedee <tdauda54@gmail.com>
2025-07-16 05:19:22 +0100

    Function Declarations and Function Expressions

--- assignment.js
+++ assignment.js
@@ -11,3 +11,23 @@ const nigeria = describeCountry("Nigeria", 200, "Abuja");
 console.log(nigeria);
 console.log(ghana);
 console.log(finland);
+
+const wordPopulation = 7900;
+function percentageOfWorld1(population) {
+  return (population / wordPopulation) * 100;
+}
+const chinaPopulation = percentageOfWorld1(1441);
+console.log(chinaPopulation);
+
+const nigeriaPopulation = percentageOfWorld1(200);
+console.log(nigeriaPopulation);
+
+const ghanaPopulation = percentageOfWorld1(30);
+console.log(ghanaPopulation);
+
+const percentageOfWorld2 = function (popultion) {
+  return (population / wordPopulation) * 100;
+};
+
+const chinaPopulation2 = percentageOfWorld1(1441);
+console.log(chinaPopulation2);
