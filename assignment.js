4d12f15569aa77dfdf1ba4054ec2767749f07fc3
emmateedee <tdauda54@gmail.com>
2025-07-19 09:19:58 +0100

    did a little practice on loops

--- assignment.js
+++ assignment.js
@@ -1,124 +1,18 @@
-"use strict";
-/*
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+'use strict';
+
+const x = 'sleeping through';
+
+const calcAge = birthYear => 2037 - birthYear;
+console.log(calcAge(1999));
+
+const str = 'emme';
+let newStr = '';
+for (let i = str.length - 1; i >= 0; i--) {
+  console.log(str[i]);
+  newStr += str[i];
 }
+console.log(newStr);
 
-const ghana = describeCountry("Ghana", 30, "Accra");
-const finland = describeCountry("Finland", 6, "Helsinki");
-const nigeria = describeCountry("Nigeria", 200, "Abuja");
-
-console.log(nigeria);
-console.log(ghana);
-console.log(finland);
-
-const wordPopulation = 7900;
-
-const chinaPopulation = percentageOfWorld1(1441);
-console.log(chinaPopulation);
-
-const nigeriaPopulation = percentageOfWorld1(200);
-console.log(nigeriaPopulation);
-
-const ghanaPopulation = percentageOfWorld1(30);
-console.log(ghanaPopulation);
-
-const percentageOfWorld2 = function (popultion) {
-  return (population / wordPopulation) * 100;
-};
-
-const chinaPopulation2 = percentageOfWorld1(1441);
-console.log(chinaPopulation2);
-
-const percentageOfWorld3 = (population) => (population / 7900) * 100;
-
-
-const describePopulation = function (country, population) {
-    const percentage = percentageOfWorld1(population);
-    return `${country} has ${population} million people, which is about ${percentage}% of the world`;
-    };
-    
-    const populationChina = describePopulation("China", 1441);
-    console.log(populationChina);
-    console.log(populations.length === 4);
-    
-    
-    const neighbours = ["Cameroon", "Niger", "Chad", "Benin republic"];
-    neighbours.push("Utopia");
-    neighbours.pop();
-    
-    if (!neighbours.includes("chad")) {
-        console.log("Probably not a west african country :D");
-        }
-    neighbours[2] = "chad republic";
-    console.log(neighbours);
-
-    
-    const myCountry = {
-        country: "Nigeria",
-        capital: "Abuja",
-        language: "english",
-        population: 200,
-        neighbours: ["Cameroon", "Niger", "Chad", "Benin republic"],
-        describe: function () {
-            return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`;
-    },
-    checkIsland: function () {
-        this.isIsland = this.neighbours.length === 0 ? true : false;
-        return this.Island;
-        },
-        };
-        
-console.log(myCountry.describe());
-console.log(myCountry.checkIsland());
-console.log(myCountry.isIsland);
-
-console.log(myCountry.population + 2);
-console.log(myCountry["population"] - 2);
-
-
-for (let i = 1; i <= 50; i++) {
-    console.log(`Voter number ${i} is currently voting`);
-    }
-    
-    
-    const listOfNeigbours = [
-  ["Canada", "Mexico"],
-  ["Spain"],
-  ["Norway", "Sweden", "Russia"],
-];
-for (let i = 0; i < listOfNeigbours.length; i++) {
-  for (let j = 0; j < listOfNeigbours[i].length; j++) {
-    console.log(`Neigbour: ${listOfNeigbours[i][j]}`);
-  }
-}
-
-
-    */
-const populations = [200, 1441, 30, 6];
-
-function percentageOfWorld1(population) {
-  return (population / 7900) * 100;
-}
-const percentages = [
-  percentageOfWorld1(populations[0]),
-  percentageOfWorld1(populations[1]),
-  percentageOfWorld1(populations[2]),
-  percentageOfWorld1(populations[3]),
-];
-console.log(percentages);
-
-const percentages2 = [];
-for (let i = 0; i < populations.length; i++) {
-  percentages2.push(percentageOfWorld1(populations[i]));
-}
-console.log(percentages2);
-
-const percentages3 = [];
-let i = 0;
-while (i < populations.length) {
-  let perc = percentageOfWorld1(populations[i]);
-  percentages3.push(perc);
-  i++;
-}
-console.log(percentages3);
+str === newStr
+  ? console.log('word is a palindrome')
+  : console.log('are you kidding me?');
