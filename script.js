1d18ef90dba6fbcb06d058d973347c380fb3df2a
emmateedee <tdauda54@gmail.com>
2025-07-20 19:12:14 +0100

    done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-"use strict";
+'use strict';
+
+function betterThanAverage(classPoints, yourPoints) {
+  let sum = 0;
+  for (let i = 0; i < classPoints.length; i++) {
+    sum += classPoints[i];
+  }
+  let avgPoints = sum / classPoints.length;
+
+  return yourPoints > avgPoints ? true : false;
+}
+console.log(betterThanAverage([5, 8, 9, 10, 13], 8));
+
+function betterThanAverage(classPoints, yourPoints) {
+  let sum = 0;
+  for (let i = 0; i < classPoints.length; i++) {
+    sum += classPoints[i];
+  }
+  let avgPoints = sum / classPoints.length;
+
+  return yourPoints > avgPoints ? true : false;
+}
+console.log(betterThanAverage([5, 8, 9, 10, 13], 8));
